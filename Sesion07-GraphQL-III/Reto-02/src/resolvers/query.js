c40f76e43d1ae5addac4388ce79ebda4e49a3c09
wicho1001 <wichoguerrero@yahoo.com>
2019-06-25 16:10:33 -0500

    Challenge 2

---
+++ Sesion07-GraphQL-III/Reto-02/src/resolvers/query.js
@@ -0,0 +1,50 @@
+import User from '../models/User';
+import Tour from '../models/Tour';
+import mongoose from 'mongoose';
+
+const Query = {
+  status: () => 'Welcome to GraphQL',
+  users: (_, args, { currentUser, userType }) => {
+    if(!currentUser) {
+      throw new Error('Unauthorized');
+    }
+
+    // Allowed only for admins
+    if (!['admin'].includes(userType)) {
+      throw new Error('Unauthorized');
+    }
+
+    return User.find().exec();
+  },
+  user: async (_, { id }, { currentUser }) => {
+    if(!currentUser) {
+      throw new Error('Unauthorized');
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new Error("Bad Request: Isn't a ObjectID")
+    }
+    const user = await User.findOne({ _id: id }).exec();
+    return user ? user : null;
+  },
+  tours: (_, { type }, { currentUser, userType }) => {
+    // Allowed all users
+    if (!['admin', 'agency', 'viewer'].includes(userType)) {
+      throw new Error('Unauthorized');
+    }
+
+    if (!currentUser) {
+      throw new Error('Unauthorized');
+    }
+
+    const filters = {};
+    if (type) {
+      filters.typeTour = type;
+    }
+
+    console.log(filters);
+
+    return Tour.find(filters).exec();
+  },
+};
+
+export default Query;
